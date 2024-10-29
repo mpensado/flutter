b288c3aac95554105d4099aba23f51d9cd82a6ba
Marco Antonio Pensado <marco.pensado@benavides.com.mx>
2024-10-29 13:18:07 -0600

    Update

---
+++ yes_no_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Deintec\Downloads\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Deintec\Downloads\Ejemplos Flutter\Personales\flutter\yes_no_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
